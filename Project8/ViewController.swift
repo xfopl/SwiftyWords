208f22d3f7ee4f49ca2e8a65136ff799419abdde
Aleksandr Veselov <fcaeoff@gmail.com>
2022-08-21 15:26:47 +0500

    Initial Commit

---
+++ Project8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Александр Веселов on 8/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
